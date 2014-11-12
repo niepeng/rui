16904565355ffcfe8f75025a1bbb5af0bb6aac9f
niepeng <pengniua@163.com>
2014-11-12 17:32:43 +0800

    add

--- ruiserver/src/main/java/com/baibutao/apps/linkshop/uxiang/ruiserver/biz/common/ContentUtil.java
+++ ruiserver/src/main/java/com/baibutao/apps/linkshop/uxiang/ruiserver/biz/common/ContentUtil.java
@@ -1,4 +1,5 @@
 package com.baibutao.apps.linkshop.uxiang.ruiserver.biz.common;
+import java.awt.SystemColor;
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,6 +9,8 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -19,6 +22,14 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.apache.commons.httpclient.util.URIUtil;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import wint.lang.utils.CollectionUtil;
+import wint.lang.utils.StringUtil;
+
+import com.baibutao.apps.linkshop.uxiang.ruiserver.biz.bean.CatchDataBean;
+import com.baibutao.apps.linkshop.uxiang.ruiserver.biz.dal.dataobject.AppInfoDO;
 
 public class ContentUtil {
 
@@ -27,7 +38,7 @@ public class ContentUtil {
 		try {
 			URL serverUrl = new URL(url);
 			HttpURLConnection conn = (HttpURLConnection) serverUrl.openConnection();
-			
+
 			conn.setConnectTimeout(timeout);
 			conn.setReadTimeout(timeout);
 			conn.setRequestMethod("GET");
@@ -50,7 +61,7 @@ public class ContentUtil {
 		return response;
 
 	}
-	
+
 	public static String getContent(String url, int timeout, String charset, boolean isStopRedirect) {
 	    String response = "";
 	    String cookie = "";
@@ -64,15 +75,15 @@ public class ContentUtil {
 	            conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
 	            conn.setInstanceFollowRedirects(false);
 	        }
-	        
+
 	        conn.setConnectTimeout(timeout);
 	        conn.setReadTimeout(timeout);
 	        conn.setRequestMethod("GET");
 	        conn.setDoOutput(true);
 	        conn.connect();
-	        
+
 	        InputStream is = conn.getInputStream();
-	        
+
 	        BufferedReader in = new BufferedReader(new InputStreamReader(is, charset));
 	        StringBuffer buffer = new StringBuffer();
 	        String line = "";
@@ -85,10 +96,10 @@ public class ContentUtil {
 	        e.printStackTrace();
 	    }
 	    return response;
-	    
+
 	}
-	
-	
+
+
 	public static String getContent(String url, String referUrl, int timeout, String charset) {
 		String response = "";
 		try {
@@ -96,7 +107,7 @@ public class ContentUtil {
 			HttpURLConnection conn = (HttpURLConnection) serverUrl.openConnection();
 			conn.setConnectTimeout(timeout);
 			conn.setReadTimeout(timeout);
-			conn.addRequestProperty("Referer", referUrl); 
+			conn.addRequestProperty("Referer", referUrl);
 			conn.setRequestMethod("GET");
 			conn.setDoOutput(true);
 			conn.connect();
@@ -117,17 +128,17 @@ public class ContentUtil {
 		return response;
 
 	}
-	
 
-    /** 
-     * 执行一个HTTP GET请求，返回请求响应的HTML 
-     * 
-     * @param url                 请求的URL地址 
-     * @param queryString 请求的查询参数,可以为null 
-     * @param charset         字符集 
-     * @param pretty            是否美化 
-     * @return 返回请求响应的HTML 
-     */ 
+
+    /**
+     * 执行一个HTTP GET请求，返回请求响应的HTML
+     *
+     * @param url                 请求的URL地址
+     * @param queryString 请求的查询参数,可以为null
+     * @param charset         字符集
+     * @param pretty            是否美化
+     * @return 返回请求响应的HTML
+     */
 	public static String doGet(String url, String queryString, String charset, boolean pretty) {
 		StringBuffer response = new StringBuffer();
 		HttpClient client = new HttpClient();
@@ -156,18 +167,18 @@ public class ContentUtil {
 			method.releaseConnection();
 		}
 		return response.toString();
-	} 
+	}
 
-    /** 
+    /**
      * 执行一个HTTP POST请求，返回请求响应的HTML
      * 建议调用该类的httpPost这个方法
-     * 
-     * @param url         请求的URL地址 
-     * @param params    请求的查询参数,可以为null 
-     * @param charset 字符集 
-     * @param pretty    是否美化 
-     * @return 返回请求响应的HTML 
-     */ 
+     *
+     * @param url         请求的URL地址
+     * @param params    请求的查询参数,可以为null
+     * @param charset 字符集
+     * @param pretty    是否美化
+     * @return 返回请求响应的HTML
+     */
 	public static String doPost(String url, Map<String, String> params, String charset, boolean pretty) {
 		StringBuffer response = new StringBuffer();
 		HttpClient client = new HttpClient();
@@ -199,9 +210,9 @@ public class ContentUtil {
 			method.releaseConnection();
 		}
 		return response.toString();
-	} 
-	
-	
+	}
+
+
 	public static String httpPost(String url, Map<String, String> params, String charset) {
 		URL u = null;
 		HttpURLConnection con = null;
@@ -257,15 +268,15 @@ public class ContentUtil {
     public static String getContent(String url, int timeout) {
         return getContent(url, timeout, "UTF-8");
     }
-    
+
     public static String getContent(String url, int timeout, boolean isStopRedirect) {
         return getContent(url, timeout, "UTF-8", isStopRedirect);
     }
-	
+
 	public static String getContent(String url, String referUrl, int timeout) {
 		return getContent(url, referUrl,timeout, "UTF-8");
 	}
-	
+
 	public static boolean saveFile(String filepathAndName, String downloadUrl) {
 		// 下载网络文件
 		int byteread = 0;
@@ -284,5 +295,71 @@ public class ContentUtil {
 		}
 		return false;
 	}
-	
+
+	public static List<AppInfoDO> catchApps(String name, int maxNum) {
+//		String url = "http://apps.wandoujia.com/api/v1/apps?ads_count=0&tag=通信聊天&max=2";
+		List<AppInfoDO> appInfoDOList = CollectionUtil.newArrayList();
+		try {
+			String url = "http://apps.wandoujia.com/api/v1/apps?ads_count=0&tag="+URLEncoder.encode(name, "utf-8")+"&max=" + maxNum;
+			String content = ContentUtil.getContent(url, 2000);
+
+			Thread.sleep(3000);
+			JSONObject jsonContentObject = new JSONObject("{\"value\":" + content + "}");
+			JSONArray tmp = JsonUtil.getJsonArray(jsonContentObject, "value");
+			JSONArray jsonArray = JsonUtil.getJsonArray(tmp.getJSONObject(0), "apps");
+			for (int i = 0; i < jsonArray.length(); i++) {
+				AppInfoDO appInfoDO = new AppInfoDO();
+
+				JSONObject currentObject = jsonArray.getJSONObject(i);
+				String mainTitle = JsonUtil.getString(currentObject, "title", null);
+				String subTitle = JsonUtil.getString(currentObject, "tagline", null);
+				int favNum = JsonUtil.getInt(currentObject, "likesCount", 0);
+				int downloadNum = JsonUtil.getInt(currentObject, "downloadCount", 0);
+
+				String info = JsonUtil.getString(currentObject, "description", null);
+				String updateInfo = JsonUtil.getString(currentObject, "changelog", null);
+				JSONObject screenshotsObject = JsonUtil.getJSONObject(currentObject, "screenshots");
+				JSONArray strArray = JsonUtil.getJsonArray(screenshotsObject, "normal");
+				String resultImgs = "";
+				for(int index=0;index <strArray.length();index++) {
+					String img = strArray.getString(index);
+					if(!StringUtil.isBlank(img)) {
+						resultImgs += (img + ",");
+					}
+				}
+
+				String iconUrl = JsonUtil.getJSONObject(currentObject,"icons").getString("px256");
+
+				JSONObject apkInfoObject = JsonUtil.getJSONObject(currentObject,"latestApk");
+//				JSONArray downloadUrlArray = apkInfoObject.getJSONArray("downloadUrls");
+//				String downloadUrl = downloadUrlArray.getJSONObject(0).getString("url");
+				String packageName = apkInfoObject.getString("packageName");
+
+				if(StringUtil.isBlank(packageName)) {
+					continue;
+				}
+
+				appInfoDO.setPackageName(packageName);
+				appInfoDO.setMainTitle(mainTitle);
+				appInfoDO.setSubTitle(subTitle);
+				appInfoDO.setFavNum(favNum);
+				appInfoDO.setDownloadNum(downloadNum);
+				appInfoDO.setScreenshots(resultImgs.substring(0, resultImgs.length() - 1));
+				appInfoDO.setInfo(info);
+				appInfoDO.setUpdateInfo(updateInfo);
+
+				CatchDataBean catchDataBean = new CatchDataBean();
+				catchDataBean.downloadUrl = "http://apps.wandoujia.com/apps/" + appInfoDO.getPackageName() + "/download";
+				catchDataBean.iconUrl = iconUrl;
+
+				appInfoDO.setCatchDataBean(catchDataBean);
+
+				appInfoDOList.add(appInfoDO);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return appInfoDOList;
+	}
+
 }
